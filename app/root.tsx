0d0fd527f92ea5227d52117ecd63d056dc9bef0c
Drew Lytle <aml3ed@virginia.edu>
2022-11-25 20:22:17 -0500

    Add tailwind

--- app/root.tsx
+++ app/root.tsx
@@ -7,6 +7,7 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import tailwindStyles from "./styles/tailwind.css";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
@@ -29,4 +30,8 @@ export default function App() {
       </body>
     </html>
   );
+}
+
+export function links() {
+  return [{ rel: "stylesheet", href: tailwindStyles }];
 }
