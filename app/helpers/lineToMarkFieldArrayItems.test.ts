1ae3647128324fcac0abf6164197b387292e5692
Drew Lytle <aml3ed@virginia.edu>
2022-12-08 18:48:16 -0500

    Add helper to convert markings to array indexed by indent

--- app/helpers/lineToMarkFieldArrayItems.test.ts
+++ app/helpers/lineToMarkFieldArrayItems.test.ts
@@ -4,13 +4,7 @@ import { lineToMarkFieldArrayItems } from "./lineToMarkFieldArrayItems";
 const defaultSong = createMockSong();
 
 describe("Array length", () => {
-  test("should convert line to an array of 30 elements minimum", () => {
-    const testLine1 = defaultSong.stanzas[0].lines[0];
-    const fieldArrayItems = lineToMarkFieldArrayItems(testLine1);
-
-    expect(fieldArrayItems.length).toBe(30);
-  });
-  test("should convert line to an array of 1 greater than length of lyrics if over 30 characters", () => {
+  test("should convert line to an array of 1 greater than length of lyrics", () => {
     const testLine = defaultSong.stanzas[0].lines[2];
     const fieldArrayItems = lineToMarkFieldArrayItems(testLine);
 
--- app/helpers/lineToMarkFieldArrayItems.ts
+++ app/helpers/lineToMarkFieldArrayItems.ts
@@ -1,8 +1,12 @@
 import type { ILine, IMarking } from "@/types/song";
 
-export function lineToMarkFieldArrayItems(line: ILine): (IMarking | null)[] {
+export function lineToMarkFieldArrayItems(
+  line: ILine
+): (IMarking | undefined)[] {
   const lyricCharactersCount = line.lyrics.length;
-  const arrayItems = new Array(Math.max(lyricCharactersCount + 1, 30));
+  const arrayItems = new Array(
+    lyricCharactersCount > 1 ? lyricCharactersCount + 1 : 30
+  ).fill(undefined);
   line.markings.forEach((marking) => {
     arrayItems[marking.indent] = marking;
   });
