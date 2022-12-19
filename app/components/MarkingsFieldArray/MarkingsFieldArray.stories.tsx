e1430b56a1c6764d88189fd20a7f5c5f88fc326b
Drew Lytle <aml3ed@virginia.edu>
2022-12-19 15:41:17 -0500

    Remove MarkingsField Array

--- app/components/MarkingsFieldArray/MarkingsFieldArray.stories.tsx
+++
@@ -1,6 +0,0 @@
-import { createMockSong } from "@/test/factories/song.factory";
-import { MarkingsFieldArray } from "./MarkingsFieldArray";
-
-export const Basic = () => (
-  <MarkingsFieldArray line={createMockSong().stanzas[0].lines[0]} />
-);
