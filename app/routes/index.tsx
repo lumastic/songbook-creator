f023bfeda95a0682afff310539868830014c6631
Keith Stolte <Keithrstolte@gmail.com>
2023-01-22 13:44:51 -0500

    Update index.tsx

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -6,7 +6,9 @@ export default function Index() {
   return (
     <div>
       <h1>Hello</h1>
-
+       <Form action="/auth/auth0?screen_hint=signup" method="post">
+         <button>Register with Auth0</button>
+       </Form>
       <Login />
 
       <Form method="post" action="/song/new">
