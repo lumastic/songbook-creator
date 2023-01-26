42d3af5e3fe3294eff1fae9e837cbce138cbad20
Keith Stolte <keithrstolte@gmail.com>
2023-01-26 12:29:55 -0500

    test: adding some basic working data in prisma added song in user id 3 and some categories

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -25,6 +25,31 @@ async function main() {
       name: "Keith Full Auth Flow",
     },
   });
+  await prisma.category.create({ data: { name: "Sea Shanties" } });
+  await prisma.category.create({ data: { name: "New American Americana" } });
+  await prisma.category.create({ data: { name: "Folk" } });
+  await prisma.category.create({ data: { name: "Protest" } });
+  await prisma.category.create({
+    data: { name: "Really makes you go hmmmmm." },
+  });
+  await prisma.category.create({
+    data: { name: "Really makes you go hummmmm." },
+  });
+
+  await prisma.song.create({
+    data: {
+      id: 1,
+      createdAt: "2023-01-26T12:41:28.070Z",
+      updatedAt: "2023-01-26T12:41:56.718Z",
+      title: "test",
+      attribution: "te",
+      stanzas:
+        '[{"type":"verse","id":"hocze1qhqldd33p4n","lines":[{"id":"hocze1qhqldd33p4o","lyrics":"te","notes":""},{"id":"ldd33sdr","lyrics":"test","notes":""}]}]',
+      published: false,
+      authorId: 3,
+    },
+  });
+  // await prisma.category.upsert
   console.log({ keith, drew, keithFullAuth });
 }
 main()
