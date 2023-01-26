1835a279b4679f7cc2a7bbab6eaa6aa9b4adf27c
Keith Stolte <Keithrstolte@gmail.com>
2023-01-26 10:30:54 -0500

    Adding Sentry.io for diagnostics and performance (#12)

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,6 +1,22 @@
-import { RemixBrowser } from "@remix-run/react";
-import { startTransition, StrictMode } from "react";
+import { RemixBrowser, useLocation, useMatches } from "@remix-run/react";
+import { startTransition, StrictMode, useEffect } from "react";
 import { hydrateRoot } from "react-dom/client";
+
+import * as Sentry from "@sentry/remix";
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  tracesSampleRate: 1,
+  integrations: [
+    new Sentry.BrowserTracing({
+      routingInstrumentation: Sentry.remixRouterInstrumentation(
+        useEffect,
+        useLocation,
+        useMatches
+      ),
+    }),
+  ],
+});
 
 function hydrate() {
   startTransition(() => {
