522cee96a89bfb7c3825050f53162134a68a08fe
Drew Lytle <aml3ed@virginia.edu>
2023-01-26 19:32:31 -0500

    First version of plop template

--- plopfile.js
+++ plopfile.js
@@ -1,4 +1,13 @@
-export default function (
+module.exports = function (
   /** @type {import('plop').NodePlopAPI} */
   plop
-) {}
+) {
+  plop.setHelper("includes", (array, string, options) => {
+    if (array.includes(string)) {
+      return options.fn(this);
+    } else {
+      return options.inverse(this);
+    }
+  });
+  plop.setGenerator("route", require("./generators/route"));
+};
