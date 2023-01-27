1b3635e57ae2e026c22e7bea783bf93ffb7ba7d4
Drew Lytle <aml3ed@virginia.edu>
2023-01-27 12:23:38 -0500

    Add model generator

---
+++ generators/model/model.js
@@ -0,0 +1,41 @@
+// Remixed 😉 from https://github.com/bradgarropy/plop-generator-remix-route/blob/master/src/route.ts
+
+module.exports = {
+  description: "💿 Remix route",
+  prompts: [
+    {
+      type: "input",
+      name: "name",
+      message: "Model name",
+    },
+    {
+      type: "checkbox",
+      name: "attributes",
+      loop: true,
+      choices: [
+        {
+          name: "id",
+          value: "id",
+          checked: true,
+        },
+        {
+          name: "createdAt",
+          value: "createdAt",
+          checked: true,
+        },
+        {
+          name: "updatedAt",
+          value: "updatedAt",
+          checked: true,
+        },
+      ],
+    },
+  ],
+  actions: [
+    {
+      type: "append",
+      path: "prisma/schema.prisma",
+      templateFile: "./generators/model/model.hbs",
+    },
+  ],
+};
