14a4426e19de9fc39bae68623275130ae18d724a
Drew Lytle <aml3ed@virginia.edu>
2023-01-27 19:41:15 -0500

    Remove netlify remix server config

--- remix.config.js
+++ remix.config.js
@@ -1,13 +1,4 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  serverBuildTarget: "netlify",
-  server:
-    process.env.NETLIFY || process.env.NETLIFY_LOCAL
-      ? "./server.js"
-      : undefined,
   ignoredRouteFiles: ["**/.*", "**/*.test.*"],
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // serverBuildPath: ".netlify/functions-internal/server.js",
-  // publicPath: "/build/",
 };
