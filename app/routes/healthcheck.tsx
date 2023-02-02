1d672b03c4950e80124f47de117dc55ea082a5d1
Drew Lytle <aml3ed@virginia.edu>
2023-02-01 20:13:57 -0500

    Update healthcheck

--- app/routes/healthcheck.tsx
+++ app/routes/healthcheck.tsx
@@ -7,14 +7,15 @@ export async function loader({ request }: LoaderArgs) {
     request.headers.get("X-Forwarded-Host") ?? request.headers.get("host");
 
   try {
-    const url = new URL("/", `http://${host}`);
     // if we can connect to the database and make a simple query
     // and make a HEAD request to ourselves, then we're good.
     await Promise.all([
       prisma.user.count(),
-      fetch(url.toString(), { method: "HEAD" }).then((r) => {
-        if (!r.ok) return Promise.reject(r);
-      }),
+      fetch(`${new URL(request.url).protocol}${host}`, { method: "HEAD" }).then(
+        (r) => {
+          if (!r.ok) return Promise.reject(r);
+        }
+      ),
     ]);
     return new Response("OK");
   } catch (error: unknown) {
