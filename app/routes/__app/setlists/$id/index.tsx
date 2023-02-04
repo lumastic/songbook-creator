b69a5f129e89e06158c3e7a5f7a20a052e74db9d
Drew Lytle <aml3ed@virginia.edu>
2023-02-04 08:48:38 -0500

    Add blank list handler for setlist songs

--- app/routes/__app/setlists/$id/index.tsx
+++ app/routes/__app/setlists/$id/index.tsx
@@ -3,6 +3,8 @@ import { Disclosure, Menu } from "@headlessui/react";
 import {
   EllipsisVerticalIcon,
   PencilIcon,
+  PencilSquareIcon,
+  PlusIcon,
   TrashIcon,
 } from "@heroicons/react/24/outline";
 import { PlayIcon } from "@heroicons/react/24/solid";
@@ -65,24 +67,10 @@ export default function ViewSetlist() {
           to={`/setlists/${setlist.id}?modal=edit-setlist`}
           className="absolute top-2 right-4 flex items-center text-xs"
           size="sm"
-          variant="outlined"
+          variant="text"
         >
           <span className="mr-2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              width={"1em"}
-              height={"1em"}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
-              />
-            </svg>
+            <PencilSquareIcon width={"1em"} />
           </span>
           Edit Details
         </Button.Link>
@@ -99,32 +87,37 @@ export default function ViewSetlist() {
       </div>
       <div className="flex">
         <h2 className="flex-1 text-2xl">Songs</h2>
-        <Button.Link
-          to="?modal=add-songs"
-          variant="secondary"
-          className="inline-flex items-center"
-        >
-          <span className="mr-2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              width={"1em"}
-              height={"1em"}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M12 4.5v15m7.5-7.5h-15"
-              />
-            </svg>
-          </span>
-          Add Songs
-        </Button.Link>
+        {!!setlist.songs.length && (
+          <Button.Link
+            to="?modal=add-songs"
+            variant="primary"
+            className="inline-flex items-center"
+          >
+            <span className="mr-2">
+              <PlusIcon width={"1em"} />
+            </span>
+            Add Songs
+          </Button.Link>
+        )}
       </div>
       <div className="space-y-4">
+        {!setlist.songs.length && (
+          <div className=" rounded-md bg-white p-8 shadow-md">
+            <h2 className="mb-4 text-2xl font-medium text-neutral-700">
+              Start adding songs to this setlist.
+            </h2>
+            <Button.Link
+              to="?modal=add-songs"
+              variant="primary"
+              className="inline-flex items-center"
+            >
+              <span className="mr-2">
+                <PlusIcon width={"1em"} />
+              </span>
+              Add Songs
+            </Button.Link>
+          </div>
+        )}
         {setlist.songs.map((song, index) => {
           function removeSong() {
             submit(
